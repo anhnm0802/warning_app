f7d8c6f1b07f3580787b021688f394e4d8d15e6c
anhnm0802 <anhnm0802@gmail.com>
2024-03-14 16:07:08 +0700

    update icon tiktok

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,11 +1,12 @@
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { faCircleXmark, faPlus, faSpinner, faUser } from '@fortawesome/free-solid-svg-icons';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react/headless';
 import 'tippy.js/dist/tippy.css'; // optional
 import styles from './header.module.scss';
 import images from '~/assets/images';
+import { faMessage, faPaperPlane, faCircle } from '@fortawesome/free-regular-svg-icons';
 
 const cx = classNames.bind(styles);
 
@@ -18,7 +19,13 @@ function Header() {
                     <img src={images.logo} alt="logo tiktok" />
                 </div>
                 {/* Search */}
-                <Tippy render={(attrs) => <div className={cx('search-result')} tabIndex="-1" {...attrs}></div>}>
+                <Tippy
+                    render={(attrs) => (
+                        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                            Ket qua
+                        </div>
+                    )}
+                >
                     <div className={cx('search')}>
                         <input placeholder="Tìm kiếm"></input>
                         {/* button X */}
@@ -34,10 +41,22 @@ function Header() {
                     </div>
                 </Tippy>
                 <div className={cx('action')}>
-                    <div className={cx('upload')}></div>
-                    <div className={cx('messenger')}></div>
-                    <div className={cx('notification')}></div>
-                    <div className={cx('profile_user')}></div>
+                    <div className={cx('upload')}>
+                        <button className={cx('upload-btn')}>
+                            <FontAwesomeIcon icon={faPlus} />
+                        </button>
+                        <span className={cx('upload-span')}>Tải lên</span>
+                    </div>
+                    <div className={cx('messenger')}>
+                        <FontAwesomeIcon icon={faPaperPlane} />
+                    </div>
+                    <div className={cx('notification')}>
+                        <FontAwesomeIcon className={cx('notification_mes')} icon={faMessage} />
+                        <FontAwesomeIcon className={cx('notification_ping')} icon={faCircle} />
+                    </div>
+                    <div className={cx('profile_user')}>
+                        <FontAwesomeIcon className={cx('profile_icon')} icon={faUser} />
+                    </div>
                 </div>
             </div>
         </header>
